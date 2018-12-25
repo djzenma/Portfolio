e6f2114dc4f258dcdf65bcd23c0469aa8c6e40b7
djzenma <eidma@aucegypt.edu>
2018-12-26 01:34:44 +0200

    Updates

---
+++ precache-manifest.54b26c1780e287ec1ae005355b27a02d.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = [
+  {
+    "revision": "e304e964d2d341dae5ffe061660b3d0f",
+    "url": "/Portfolio/static/media/myImg.e304e964.jpg"
+  },
+  {
+    "revision": "e327160ba23d1bc7c457b63798b85a69",
+    "url": "/Portfolio/static/media/background4.e327160b.jpg"
+  },
+  {
+    "revision": "246ecd228024221d710d18c922083e06",
+    "url": "/Portfolio/static/media/Topffer.246ecd22.jpg"
+  },
+  {
+    "revision": "e97fba075297c038c98bf76718d0c5b3",
+    "url": "/Portfolio/static/media/AUC Library.e97fba07.jpg"
+  },
+  {
+    "revision": "3b96116072eb85f5a3a2",
+    "url": "/Portfolio/static/js/runtime~main.3b961160.js"
+  },
+  {
+    "revision": "131c9b188b885221420e",
+    "url": "/Portfolio/static/js/main.131c9b18.chunk.js"
+  },
+  {
+    "revision": "5f752d4c7bac9985ca5d",
+    "url": "/Portfolio/static/js/1.5f752d4c.chunk.js"
+  },
+  {
+    "revision": "131c9b188b885221420e",
+    "url": "/Portfolio/static/css/main.5ca107d8.chunk.css"
+  },
+  {
+    "revision": "5f752d4c7bac9985ca5d",
+    "url": "/Portfolio/static/css/1.7a9a0aea.chunk.css"
+  },
+  {
+    "revision": "c84ed84fd8036762858be3d8b0363381",
+    "url": "/Portfolio/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/Portfolio/precache-manifest.7d7293076ac8b22fd3b5ff428efe013c.js"
+  "/Portfolio/precache-manifest.54b26c1780e287ec1ae005355b27a02d.js"
 );
 
 workbox.clientsClaim();
